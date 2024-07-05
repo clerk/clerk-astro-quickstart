87a6ac7528179a192d134b393090477d61941f96
Roy Anger <roy@royanger.com>
2024-07-04 23:27:12 -0400

    Integrated Clerk into application

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,4 +1,12 @@
 import { defineConfig } from 'astro/config';
+import node from "@astrojs/node";
+import clerk from '@clerk/astro';
 
 // https://astro.build/config
-export default defineConfig({});
+export default defineConfig({
+  integrations: [clerk()],
+  output: "server",
+  adapter: node({
+    mode: "standalone"
+  })
+});
